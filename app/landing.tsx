cb7536f095ef1fdd4e87e4b2e071348e23ec364f
VArtzy <farrelnikoson@gmail.com>
2023-10-30 23:41:43 +0700

    fix route

--- app/landing.tsx
+++ app/landing.tsx
@@ -12,12 +12,12 @@ export const Landing = () => {
                 alt="Landing image"
                 src="/landing.png"
             />
-            )
             <Link href="/">
                 <Button className="absolute uppercase bg-amber-500 top-1/2 left-1/3">
                     Mulai Belajar
                 </Button>
             </Link>
+            )
         </>
     )
 }
